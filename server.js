f3fc161d54da69511820c2ec064982668dbd6831
Elias Saffarmoghadam <esaffarmoghadam@gmail.com>
2026-03-16 17:47:02 +0100

    init node.js learning by Dave Grey

---
+++ server.js
@@ -0,0 +1,11 @@
+console.log("Server is running...");
+const math = require("./math");
+console.log(math.add(5,3));
+// console.log(global);
+
+// // const os = require("os");
+// const path = require("path");
+// console.log(path.dirname(__filename));
+// console.log(path.dirname(__dirname));
+
+// console.log(path.parse(__filename));

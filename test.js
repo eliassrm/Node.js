df11e3c7e99ae69a4b2f186a198acb7725ccb910
Elias Saffarmoghadam <esaffarmoghadam@gmail.com>
2026-03-19 12:59:57 +0100

    Update index.html and test.js: comment out unused script and add test functionality

--- test.js
+++ test.js
@@ -1,1 +1,12 @@
-alert("hello world");
+
+let x = Math.random() * 68+1;
+console.log(floor(x));
+console.log("This is a test file to check if \
+     the script is linked correctly.".charAt(x));
+     
+    
+console.log("This is a test file to check if \
+     the script is linked correctly.".length);     
+
+
+

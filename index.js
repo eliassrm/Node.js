c5876036c73d11c686cc11f4c6f955abd0eb9885
Elias Saffarmoghadam <esaffarmoghadam@gmail.com>
2026-03-24 10:18:09 +0100

    init node -i

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-// This is the read file 
-const fs = require('fs');
-const path = require('path');
+const { format } = require('date-fns');
 
-fs.readFile(path.join(__dirname, 'files', 'start_here.txt'),'utf8', (err, data)=>{
-    if(err) throw err;
-    console.log(data);
-    console.log(data.toString());
-})
-
-console.log("This will be printed first?");
-//exit on uncaught exceptions
-process.on('uncaughtException', err =>{
-    console.error('There was an uncaught error', err);
-    process.exit(1); //mandatory (as per the Node.js docs)
-})
-
-console.log("This will be printed");
+console.log(format(new Date(), 'yyyy-MM-d\tHH:mm:ss'));

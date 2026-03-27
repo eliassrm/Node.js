85060cbe0a8d00b37b31aa78e67bd129fa3e08b5
Elias Saffarmoghadam <esaffarmoghadam@gmail.com>
2026-03-27 18:17:05 +0100

    d

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
-console.log("Server is running...");
-const math = require("./math");
-console.log(math.add(5,3));
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+const fsPromises = require("fs").promises;
 
-console.log(math.divide(10,3.3));
+console.log("Server is running...");
+
+const logEvents = require("./logEvents");
+const EventEmitter = require("events");
+class Emitter extends EventEmitter {};
+// Initialize the event object
+const myEmitter = new Emitter();
+
+const PORT = process.env.PORT || 3500;
+
+const server = http.createServer((req, resp) => {
+    console.log(req.url, req.method);
+
+});
+
+server.listen(PORT, ()) => console.log(`Server is running on port ${PORT}...`));
+
 // // const os = require("os");
 // const path = require("path");
 // console.log(path.dirname(__filename));

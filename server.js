0a97632c40cdb9e6667dc9143c809e3f1db912b5
Elias Saffarmoghadam <esaffarmoghadam@gmail.com>
2026-04-07 16:35:36 +0200

    node.js course tille 1:38:00 by Dave Grey

--- server.js
+++ server.js
@@ -15,10 +15,43 @@ const PORT = process.env.PORT || 3500;
 
 const server = http.createServer((req, resp) => {
     console.log(req.url, req.method);
+    
+    const extension = path.extname(req.url);
+    let contentType;
 
+    switch (extension) {
+        case ".css":
+            contentType = "text/css";
+            break;
+        case ".js":
+            contentType = "text/javascript";
+            break;
+        case ".json":
+            contentType = "application/json";
+            break;
+        case ".jpg":
+            contentType = "image/jpeg";
+            break;
+        case ".png":
+            contentType = "image/png";
+            break;
+        default:
+            contentType = "text/html";
+    }
+
+    let filePath =
+        contentType === "text/html" && req.url === "/"
+            ? path.join(__dirname, "views", "index.html")
+            : contentType === "text/html" && req.url.slice(-1) === "/"
+                ? path.join(__dirname, "views", req.url, "index.html")
+                : contentType === "text/html"
+                    ? path.join(__dirname, "views", req.url)
+                    : path.join(__dirname, req.url);
+    
+    
 });
 
-server.listen(PORT, ()) => console.log(`Server is running on port ${PORT}...`));
+server.listen(PORT, () => console.log(`Server is running on port ${PORT}...`));
 
 // // const os = require("os");
 // const path = require("path");
